6a43e68d76cc25f64dad010c417012f3aa2432e8
Julia Martinez <zeusgirl99@gmail.com>
2023-03-05 14:37:45 -0500

    Halfway save

---
+++ src/tuiter/navigation-sidebar/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+const NavigationSidebar = (
+    {
+        active = 'explore'
+    }
+) => {
+    return( <div className="list-group">
+            <a className="list-group-item">Tuiter</a>
+            <a className="list-group-item">Home</a>
+            <a className="list-group-item">Explore</a>
+            <a className="list-group-item">Notifications</a>
+            <a className="list-group-item">Messages</a>
+            <a className="list-group-item">Bookmarks</a>
+            <a className="list-group-item">Lists</a>
+            <a className="list-group-item">Profile</a>
+            <a className="list-group-item">More</a>
+        </div>
+)};
+export default NavigationSidebar;
